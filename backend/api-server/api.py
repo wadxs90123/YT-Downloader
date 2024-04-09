738cd71488ecbfca13bc521715ca209509bf5482
Siang <85378478+wadxs90123@users.noreply.github.com>
2024-04-09 17:35:38 +0800

    Update api.py
    1. Modify change the "/download" API method to POST, modify input parameter of "download".
    2. Add a GET API "/" for testing .
    3. Add POST API "/video/{video_id}" for upload streaming to user.

--- backend/api-server/api.py
+++ backend/api-server/api.py
@@ -1,7 +1,11 @@
 import os, sys
-sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "..")) + "\\yt-downloader")
-from fastapi import FastAPI
-from downloader import downloadVideo
+from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__)) 
+sys.path.append(os.path.join(current_dir, '../yt-downloader'))
+from downloader import downloadVideo 
 
 app = FastAPI()
 
@@ -16,7 +20,41 @@ app.add_middleware(
 
 # test
 @app.get("/")
-def download(url: str, type: str, outputPath=None):
-    name, path = downloadVideo(url, type, outputPath)
+def hello_world():
+    return {"message": "Hello World"}
 
-    return {f"Video downloaded successfully / Name: {name} / Path: {path}"}
+# download in the backend
+@app.post("/download")
+def download(url: str, type: str):
+    # 以固定的path去實作 
+    name, path = downloadVideo(url, type, "./videos/")
+    
+    return {"message": "Video downloaded successfully",
+            "name":{name}
+            }
+ 
+# upload to user 
+@app.post("/video/{video_id}")
+async def read_item(video_id: str):
+    headers = {}
+    video_content = None
+    if video_id.endswith(".mp4"):
+        headers["Content-Type"] = "video/mp4"
+        headers["Content-Disposition"] = "attachment; filename=video.mp4"
+        with open(f"./videos/{video_id}", "rb") as video_file:
+            video_content = video_file.read()
+        return Response(content=video_content, headers=headers)
+    elif video_id.endswith(".wmv"):
+        headers["Content-Type"] = "video/x-ms-wmv"
+        headers["Content-Disposition"] = "attachment; filename=video.wmv"
+        with open(f"./videos/{video_id}", "rb") as video_file:
+            video_content = video_file.read()
+        return Response(content=video_content, headers=headers)
+    elif video_id.endswith(".mp3"):
+        headers["Content-Type"] = "video/mp3"
+        headers["Content-Disposition"] = "attachment; filename=video.mp3"
+        with open(f"./videos/{video_id}", "rb") as video_file:
+            video_content = video_file.read()
+        return Response(content=video_content, headers=headers)
+    else:
+        return {"message": "Invalid video type"}
