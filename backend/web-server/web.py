e96a3c83ba817254291d3fe966a1801b42fe323c
Siang <85378478+wadxs90123@users.noreply.github.com>
2024-04-09 17:39:07 +0800

    Removing useless codes.

--- backend/web-server/web.py
+++ backend/web-server/web.py
@@ -1,33 +1,24 @@
 from flask import Flask, render_template, request, Response, jsonify
 import time
+import sys 
+import os  
+
+#TODO Garbage collection (remove downloaded files)
+
+current_dir = os.path.dirname(os.path.abspath(__file__)) 
+sys.path.append(os.path.join(current_dir, '../api-server'))
+ 
+ 
 
 app = Flask(__name__)
+
 
 # 首頁
 @app.route('/')
 def index():
-    return render_template('download.html')
-
-@app.route('/download', methods=['POST'])
-def download():
-    if request.method == 'POST':
-        url = request.form.get('url')
-        if url:
-            print(f'下載影片： {url}')
-            
-            return jsonify({'status': 'success', 'url': url})
-        else:
-            return jsonify({'status': 'error', 'message': 'Missing URL parameter'})
-    else:
-        return jsonify({'status': 'error', 'message': 'Method Not Allowed'}), 405
-    
-@app.route('/progress', methods=['GET'])
-def progress():
-    def generate():
-        for i in range(101):
-            time.sleep(0.1)
-            yield f"data:{i}\n\n"
-    return Response(generate(), mimetype='text/event-stream')
+    return render_template('index.html')
+ 
+ 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  
