b356db4d98b6ab0d3d43d2f29a70ba4bf228adeb
Siang <85378478+wadxs90123@users.noreply.github.com>
2024-04-09 17:30:21 +0800

    Add CORS middleware,

--- backend/api-server/api.py
+++ backend/api-server/api.py
@@ -5,6 +5,16 @@ from downloader import downloadVideo
 
 app = FastAPI()
 
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5000"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
+
+# test
 @app.get("/")
 def download(url: str, type: str, outputPath=None):
     name, path = downloadVideo(url, type, outputPath)
